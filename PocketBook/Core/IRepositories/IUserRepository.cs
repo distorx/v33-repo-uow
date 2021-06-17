c367e0a5f6983a5c530d1787070882dbf0520fe5
mohamad <mohamad.lawand@outlook.com>
2021-06-17 11:55:05 +0100

    initial commit

---
+++ PocketBook/Core/IRepositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Threading.Tasks;
+using PocketBook.Models;
+
+namespace PocketBook.Core.IRepositories
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+    }
+}
---
+++ PocketBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using PocketBook.Models;
+
+namespace PocketBook.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // the dbset property will tell ef core that we have
+        // a table that needs to be created if doesnt exist
+        public virtual DbSet<User> Users {get;set;}
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
